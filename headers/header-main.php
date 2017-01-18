5fbc04a178f71964dc772cf958eca211509f1d7a
archivescode <archivescode@gmail.com>
2017-01-18 09:54:42 +0700

    heade dan navigasi

--- headers/header-main.php
+++ headers/header-main.php
@@ -1,29 +1,117 @@
+	<?php
+	$column_logo = 'col-sm-4';
+	$column_nav = 'col-sm-8';
+	$top = 'false';
+	$left = 'false';
+	if ( function_exists('fw_get_db_settings_option')){
+		if (!empty(fw_get_db_settings_option('nav_position'))){
+			$nav_position = fw_get_db_settings_option('nav_position');
+			if($nav_position == 'right'){
+				$column_logo = 'col-sm-4';
+				$column_nav = 'col-sm-8';
+				$top = 'false';
+				$left = 'false';
+			}elseif ($nav_position == 'left') {
+				$column_logo = 'col-sm-4';
+				$column_nav = 'col-sm-8';
+				$top = 'false';
+				$left = 'true';
+			}elseif ($nav_position == 'bottom') {
+				$column_logo = 'col-sm-12';
+				$column_nav = 'col-sm-12';
+				$top = 'false';
+				$left = 'false';
+			}elseif ($nav_position == 'top') {
+				$column_logo = 'col-sm-12';
+				$column_nav = 'col-sm-12';
+				$top = 'true';
+				$left = 'false';
+			}
+		}
+	}
+	?>
+	<?php
+if ($top == 'true' || $left == 'true'){ ?>
 	<div class="container">
-	<div class="row">
-		<div class="col-xs-12 col-sm-4">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<div class="row">
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+			<div class="col-xs-12 <?php echo $column_nav ?>">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'poc' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+			</div>
+
+			<div class="col-xs-12 <?php echo $column_logo ?>">
+			<div class="site-branding">
+				<?php
+
+				//Logo
+
+				if ( function_exists('fw_get_db_settings_option') && !empty(fw_get_db_settings_option('logo')) ) {
+					$logo = fw_get_db_settings_option('logo');
+					echo '<div class="site-logo"><a href="' . esc_url( home_url( '/' ) ) . '" rel="home"><img src="' . $logo['url'] . '" /></a></div>';
+
+				}else{
+
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+					endif;
+				}
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<div class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></div>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
+			</div>
+
 		</div>
-
-		<div class="col-xs-12 col-sm-8">
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'poc' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-		</div>
-
 	</div>
-</div>
+<?php }else{ ?>
+
+		<div class="container">
+		<div class="row">
+			<div class="col-xs-12 <?php echo $column_logo ?>">
+			<div class="site-branding">
+				<?php
+
+				//Logo
+
+				if ( function_exists('fw_get_db_settings_option') && !empty(fw_get_db_settings_option('logo')) ) {
+					$logo = fw_get_db_settings_option('logo');
+					echo '<div class="site-logo"><a href="' . esc_url( home_url( '/' ) ) . '" rel="home"><img src="' . $logo['url'] . '" /></a></div>';
+
+				}else{
+
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+					endif;
+				}
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<div class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></div>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
+			</div>
+
+			<div class="col-xs-12 <?php echo $column_nav ?>">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'poc' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+			</div>
+
+		</div>
+	</div>
+
+<?php } ?>
--- inc/styles.php
+++ inc/styles.php
@@ -17,6 +17,7 @@
 				echo "#page{-webkit-box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.5);-moz-box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.5);box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.5);width:".$site_width.";margin:0 auto;}";
 			}
 
+			//header top style
 			$show_header_top = fw_get_db_settings_option('show_header_top');
 			if ($show_header_top == 'no') {
 				echo '.header-top-wrap{display:none;}';
@@ -25,6 +26,24 @@
 
 			if(!empty($header_top_border_color)){
 				echo 'header .header-top-wrap{border-top-color: '.$header_top_border_color.'}';
+			}
+
+			$header_top_background = fw_get_db_settings_option('header_top_background');
+
+			if(!empty($header_top_background)){
+				echo 'header .header-top-wrap{background: '.$header_top_background.'}';
+			}
+			//header option
+			$show_description = fw_get_db_settings_option('show_description');
+
+			if(!empty($show_description) && $show_description == 'no'){
+				echo 'header .site-description{display: none;}';
+			}
+
+			$logo_height = fw_get_db_settings_option('logo_height');
+
+			if(!empty($show_description)){
+				echo 'header .site-logo a img{height: '. $logo_height .';}';
 			}
 
 			//sidebar style
